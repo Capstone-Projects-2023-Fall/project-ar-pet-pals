8f6d8c96b86c8d330f529390964b2aa0cb51afe2
dariodaguanno <tul11082@temple.edu>
2023-10-15 21:12:44 -0400

    Created Unit Tests

---
+++ ARPetPals/Assets/PlayModeTests/PlayModeTestScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class PlayModeTestScript
+{
+    [Test]
+    public void MyTestScriptsSimplePasses() {
+        // Use the Assert class to test conditions
+
+        Assert.AreEqual(1, 1);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator MyTestScriptsWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+    [Test]
+    public void TestEqualNum() {
+        Assert.AreEqual(1, 1);
+    }
+    [Test]
+    public void TestNotEqualNum() {
+        Assert.AreNotEqual(1, 2);
+    }
+
+}
