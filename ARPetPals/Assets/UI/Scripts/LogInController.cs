11faee2eb40f90de81663f7baf2e8fbbcf196541
hynguyen33 <tug63287@temple.edu>
2023-10-21 01:07:26 -0400

    completed sign-in and sign-up scene

---
+++ ARPetPals/Assets/UI/Scripts/LogInController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LogInController : MonoBehaviour
+{
+    public string usernameInput;
+    public string passwordInput;
+    public GameObject gameObject;
+    [SerializeField] 
+    public TMP_Text EditText;
+    
+    public void getUserName(string s)
+    {
+        usernameInput = s;
+        Debug.Log("username: " + usernameInput);
+    }
+
+    public void getPassword(string s)
+    {
+        passwordInput = s;
+        Debug.Log("password: " + passwordInput);
+    }
+
+    public void loginButtonClicked()
+    {
+        EditText.text = "get user name and password: " + usernameInput + " " + passwordInput;
+        Debug.Log("Log in Clicked");
+        // if (passwordInput != null && usernameInput != null)
+        // {
+        //     EditText.text = "get user name and password: " + usernameInput + " " + passwordInput;
+        //     Debug.Log("Log in Clicked");
+        // }
+    }
+
+    public void registerButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    
+}
