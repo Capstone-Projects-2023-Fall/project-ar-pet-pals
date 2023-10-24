a92756b455500cd96a3008a3a416936cc908250f
hynguyen33 <tug63287@temple.edu>
2023-10-24 12:40:48 -0400

    change font, input function for input field of sign in and signup scene

--- ARPetPals/Assets/UI/Scripts/SignUpController.cs
+++ ARPetPals/Assets/UI/Scripts/SignUpController.cs
@@ -8,33 +8,59 @@ using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject gameObject;
-    public string usernameInput;
-    public string passwordInput;
-    public string confirmPasswordInput;
+    public string usernameInput = "" ;
+    public string passwordInput = "";
+    public string confirmPasswordInput = "";
     public TMP_Text editText;
+    public string errormessage = "";
+    public TMP_InputField usernameField;
+    public TMP_InputField passwordField;
+    public TMP_InputField confirmPasswordField;
 
-    public void getUsername(string s)
-    {
-        usernameInput = s;
-        Debug.Log("username: " + usernameInput);
-    }
-
-    public void getPassword(string s)
-    {
-        passwordInput = s;
-        Debug.Log("password: " + passwordInput);
-    }
-
-    public void getConfirmPassword(string s)
-    {
-        confirmPasswordInput = s;
-        Debug.Log("confirma password input: " + confirmPasswordInput);
-    }
+    // public void getUsername(string s)
+    // {
+    //     usernameInput = s;
+    //     Debug.Log("username: " + usernameInput);
+    // }
+    //
+    // public void getPassword(string s)
+    // {
+    //     passwordInput = s;
+    //     Debug.Log("password: " + passwordInput);
+    // }
+    //
+    // public void getConfirmPassword(string s)
+    // {
+    //     confirmPasswordInput = s;
+    //     Debug.Log("confirm password input: " + confirmPasswordInput);
+    // }
 
     public void registerButtonClicked()
     {
-        editText.text = passwordInput.Equals(confirmPasswordInput) ? "Password correct" : "Password incorrect";
+        usernameInput = usernameField.text;
+        passwordInput = passwordField.text;
+        confirmPasswordInput = confirmPasswordField.text;
+        
+        if (usernameInput == "")
+        {
+            errormessage += " username\n";
+        }
+        if (passwordInput == "")
+        {
+            errormessage += "password\n";
+        }
+        if (confirmPasswordInput == "")
+        {
+            errormessage += "confirm password";
+        }
+        while (errormessage != "")
+        {
+            editText.text = $"Missing\n{errormessage}.";
+            Debug.Log($"Error message {errormessage}");
+        }
+        
         Debug.Log("register clicked");
+        Debug.Log($"Sign up Clicked username: {usernameInput} password: {passwordInput} confirm password: {confirmPasswordInput}");
         if (usernameInput != "" && passwordInput != "" && confirmPasswordInput != "" &&
             passwordInput.Equals(confirmPasswordInput))
         {
