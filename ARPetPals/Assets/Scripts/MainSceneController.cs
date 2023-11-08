774fb25e576378609c0ed455af8f4039592c6115
dariodano <tul11082@temple.edu>
2023-11-08 16:40:00 -0500

    Pet transitions to specified area when tap and hold

--- ARPetPals/Assets/Scripts/MainSceneController.cs
+++ ARPetPals/Assets/Scripts/MainSceneController.cs
@@ -14,8 +14,6 @@ public class MainSceneController : MonoBehaviour
 
     [SerializeField] private TMP_Text petNameDisplay;
 
-    public GameObject selectedCharacter;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +22,6 @@ public class MainSceneController : MonoBehaviour
         Debug.Log("Pet choosen to load is: "+ characterChoiceIndex);
 
         characterList = new GameObject[transform.childCount];
-
-        GameObject selectedCharacter = new GameObject();
 
         //fill array with our dragon models
         for (int i = 0; i < transform.childCount; i++) {
@@ -40,11 +36,7 @@ public class MainSceneController : MonoBehaviour
         
         if (characterList[characterChoiceIndex]) {
             characterList[characterChoiceIndex].SetActive(true);
-            selectedCharacter = characterList[characterChoiceIndex];
         }
-
-        //GameObject newObject = Instantiate(characterList[characterChoiceIndex]);
-        //newObject.transform.position = new Vector3(0, 0, 0);
 
         petNameDisplay.text = PlayerPrefs.GetString("CustomName");
     }
--- ARPetPals/Assets/Scripts/PlaceObjects/PlaceOnPlane.cs
+++ ARPetPals/Assets/Scripts/PlaceObjects/PlaceOnPlane.cs
@@ -5,7 +5,7 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.InputSystem;
 
-
+//make sure there is ARRaycastManager
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceOnPlane : PressInputBase {
 
@@ -13,14 +13,23 @@ public class PlaceOnPlane : PressInputBase {
     [SerializeField] GameObject objectToMove;
 
     // If there is any touch input.
-    bool isPressed;
+    private bool isPressed;
+
+    //vector where the user taps
+    private Vector3 targetPosition;
 
     ARRaycastManager aRRaycastManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
+    //adjusts the speed at which the object moves
+    private float moveSpeed = 5.0f;
+
+    private Animator animator;
+
     protected override void Awake() {
         base.Awake();
         aRRaycastManager = GetComponent<ARRaycastManager>();
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,17 +47,22 @@ public class PlaceOnPlane : PressInputBase {
             // Raycast hits are sorted by distance, so the first hit means the closest.
             var hitPose = hits[0].pose;
 
-            // Move the object to the touch position.
-            objectToMove.transform.position = hitPose.position;
-            objectToMove.transform.rotation = hitPose.rotation;
-
             // To make the object always look at the camera. Delete if not needed.
             
             Vector3 lookPos = Camera.main.transform.position - objectToMove.transform.position;
             lookPos.y = 0;
             objectToMove.transform.rotation = Quaternion.LookRotation(-lookPos);
-            
+           
+            // Set the target position for smooth movement.
+            targetPosition = hitPose.position;
         }
+
+        //animator.SetBool("isMoving", true);
+        // Smoothly move the object towards the target position.
+        objectToMove.transform.position = Vector3.Lerp(objectToMove.transform.position, targetPosition, Time.deltaTime * moveSpeed);
+
+        //animator.SetBool("isMoving", false);
+
     }
 
     protected override void OnPress(Vector3 position) => isPressed = true;
