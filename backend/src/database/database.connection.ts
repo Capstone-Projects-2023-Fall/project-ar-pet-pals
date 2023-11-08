70063a7355ec87c295561565bac3434c5ed61bae
karljamesgray <karljamesgray@gmail.com>
2023-11-08 23:11:34 +0000

    backend server now reads db credentials from env var

--- backend/src/database/database.connection.ts
+++ backend/src/database/database.connection.ts
@@ -2,7 +2,10 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
 const client = new MongoClient();
 
-const dbString = "REMOVED_ON_PURPOSE";
+const dbString = Deno.env.get("DB_CRED");
+if (!dbString) throw new Error('DB_CRED environment variable not set.');
+
+console.log(dbString);
 
 await client.connect(dbString);
 
