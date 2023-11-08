87a2c523531fe3c3710a2bd569c9b9b867e25a6c
dariodano <tul11082@temple.edu>
2023-11-08 17:25:36 -0500

    Removed incorrect animator code

--- ARPetPals/Assets/Scripts/PlaceObjects/PlaceOnPlane.cs
+++ ARPetPals/Assets/Scripts/PlaceObjects/PlaceOnPlane.cs
@@ -24,12 +24,9 @@ public class PlaceOnPlane : PressInputBase {
     //adjusts the speed at which the object moves
     private float moveSpeed = 5.0f;
 
-    private Animator animator;
-
     protected override void Awake() {
         base.Awake();
         aRRaycastManager = GetComponent<ARRaycastManager>();
-        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -47,8 +44,7 @@ public class PlaceOnPlane : PressInputBase {
             // Raycast hits are sorted by distance, so the first hit means the closest.
             var hitPose = hits[0].pose;
 
-            // To make the object always look at the camera. Delete if not needed.
-            
+            // To make the object always look at the camera. Delete if not needed.            
             Vector3 lookPos = Camera.main.transform.position - objectToMove.transform.position;
             lookPos.y = 0;
             objectToMove.transform.rotation = Quaternion.LookRotation(-lookPos);
@@ -57,12 +53,8 @@ public class PlaceOnPlane : PressInputBase {
             targetPosition = hitPose.position;
         }
 
-        //animator.SetBool("isMoving", true);
         // Smoothly move the object towards the target position.
         objectToMove.transform.position = Vector3.Lerp(objectToMove.transform.position, targetPosition, Time.deltaTime * moveSpeed);
-
-        //animator.SetBool("isMoving", false);
-
     }
 
     protected override void OnPress(Vector3 position) => isPressed = true;
