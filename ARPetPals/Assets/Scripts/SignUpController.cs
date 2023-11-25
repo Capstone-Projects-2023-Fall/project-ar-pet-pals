250e690eaea50db339f7d9bb2b42c72838b27529
Anya Tewari <78163717+atewari18@users.noreply.github.com>
2023-11-24 23:48:23 -0500

    Update SignUpController.cs
    added asking user for birthday bc i wanted to set up birthday notification

--- ARPetPals/Assets/Scripts/SignUpController.cs
+++ ARPetPals/Assets/Scripts/SignUpController.cs
@@ -4,19 +4,20 @@ using ARPetPals;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using ARPetPals.Notifications; //import Notifications namespace
-//public class NewBehaviourScript : MonoBehaviour
-public class SignUpController : MonoBehaviour  //updated class name
+
+public class SignUpController : MonoBehaviour
 {
     [SerializeField] public TMP_Text editText;
     [SerializeField] public TMP_InputField usernameField;
     [SerializeField] public TMP_InputField passwordField;
     [SerializeField] public TMP_InputField confirmPasswordField;
+    [SerializeField] public TMP_InputField birthdateField;  // Added birthdate field
     
     public GameObject gameObject;
     public string usernameInput = "";
     public string passwordInput = "";
     public string confirmPasswordInput = "";
+    public string birthdateInput = "";  // Added birthdate input
     public string errormessage = "";
 
     public void BackButtonClicked()
@@ -29,6 +30,7 @@ public class SignUpController : MonoBehaviour  //updated class name
         usernameInput = usernameField.text;
         passwordInput = passwordField.text;
         confirmPasswordInput = confirmPasswordField.text;
+        birthdateInput = birthdateField.text;  // Get birthdate input
         
         if (usernameInput == "")
         {
@@ -50,47 +52,43 @@ public class SignUpController : MonoBehaviour  //updated class name
             }
             errormessage += " confirm password";
         }
+        if (birthdateInput == "") //asking birthdate 
+        {
+            if (errormessage != "")
+            {
+                errormessage += " and";
+            }
+            errormessage += " birthdate";
+        }
 
         if (errormessage != "")
         {
             editText.text = $"Missing{errormessage}.";
-            editText.color =  Color.red;
+            editText.color = Color.red;
         }
         
         errormessage = "";
         Debug.Log($"Error message {errormessage}");
         
         Debug.Log("register clicked");
-        Debug.Log($"Sign up Clicked username: {usernameInput} password: {passwordInput} confirm password: {confirmPasswordInput}");
-        if (usernameInput != "" && passwordInput != "" && confirmPasswordInput != "" &&
+        Debug.Log($"Sign up Clicked username: {usernameInput} password: {passwordInput} confirm password: {confirmPasswordInput} birthdate: {birthdateInput}");
+        //added birthdate part to function
+        if (usernameInput != "" && passwordInput != "" && confirmPasswordInput != "" && birthdateInput != "" &&
             passwordInput.Equals(confirmPasswordInput))
         {
-            // Check if it's the initial registration
-            bool isInitialRegistration = NotificationScheduler.shouldSendInitialNotification(usernameInput);
-            // Register the user and handle notifications
-            gameObject.GetComponent<APIService>().SignUp(usernameInput, passwordInput, (errMessage) =>
+            gameObject.GetComponent<APIService>().SignUp(usernameInput, passwordInput, birthdateInput, (errMessage) =>
             {
                 if (errMessage != "")
                 {
                     Debug.Log("Signup Fail: " + errMessage);
                     editText.text = errMessage;
-                    editText.color = Color.red;
                 }
                 else
                 {
                     Debug.Log("Signup Success: " + errMessage);
-
-                    //only schedule the initial notification if it's their first registration
-                    if (isInitialRegistration)
-                    {
-                        //schedule initial notification
-                        NotificationScheduler.ScheduleInitialNotification(usernameInput);
-                    }
-
-                    SceneManager.LoadScene("PetChoiceScene");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
                 }
             });
         }
     }
 }
-        
