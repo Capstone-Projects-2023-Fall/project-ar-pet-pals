663fc9d2a36d8a50c3a1dae8c63ce5a8cff66f05
Anya Tewari <78163717+atewari18@users.noreply.github.com>
2023-11-28 01:53:18 -0500

    Create notificationScheduler.ts

---
+++ backend/src/schedulers/notificationScheduler.ts
@@ -0,0 +1,15 @@
+import { leaderboardList } from "./controllers.leaderboard.ts";
+
+// Pseudo-code for a function that sends notifications to the top 5 users
+function sendTop5Notifications(top5Users: { username: string; score: number }[]) {
+  for (const user of top5Users) {
+    // Use a notification library or service to send a notification to each user
+    sendNotification(user.username, "Congrats, you are in the top 5 this week! Open the app to celebrate with your pet!");
+  }
+}
+
+// Pseudo-code for scheduling notifications (using setInterval)
+setInterval(async () => {
+  const { top5Users } = await leaderboardList(); // Get the current top 5 users
+  sendTop5Notifications(top5Users);
+}, /* Set an appropriate interval in milliseconds, e.g., one week */);
