6b054e6ffa0d4eaf9433d92aa33510411282db5d
Anya Tewari <78163717+atewari18@users.noreply.github.com>
2023-11-28 02:24:40 -0500

    Update NotificationScheduler.cs
    sending notification warning of account termination for low activity users, using lines 269-270 of pet controller file. Sends after 30 days of not feeding or interacting w ur pet

--- ARPetPals/Assets/Scripts/Notifications/NotificationScheduler.cs
+++ ARPetPals/Assets/Scripts/Notifications/NotificationScheduler.cs
@@ -1,3 +1,8 @@
+using { calculateTimeDifferenceInMinutes } from "./yourUtilityFile.ts";
+using { Pets } from "./controllers.pets.ts";
+using Unity.Notifications.Android;
+using System;
+
 public static class NotificationScheduler
 {
   private void ScheduleNotification(int healthScore)
@@ -39,4 +44,51 @@ public static class NotificationScheduler
 
     return TimeSpan.FromHours(calculatedInterval);
 }
+  // Pseudo-code for checking account activity and sending notifications
+async function checkAccountActivityAndSendNotifications() {
+  const users = await Users.find({}).toArray(); // Retrieve all users
+
+  for (const user of users) {
+    const pet = await Pets.findOne({ user_id: user._id.toString() });
+
+    if (!pet) {
+      continue; // Skip users without pets
+    }
+
+    const minutesSinceLastActivity = calculateTimeDifferenceInMinutes(pet.status.lastActivity);
+
+    // Check if the user has low/no account activity (30 days threshold)
+    const thirtyDaysInMinutes = 30 * 24 * 60; // 30 days * 24 hours/day * 60 minutes/hour
+    if (minutesSinceLastActivity > thirtyDaysInMinutes) {
+      // Send a warning notification
+      sendNotification(user.username, "Warning: Your account is at risk of termination. Log in to feed your pet!");
+    }
+  }
 }
+
+// Pseudo-code for scheduling the periodic task
+setInterval(async () => {
+  await checkAccountActivityAndSendNotifications();
+}, /* Set an appropriate interval in milliseconds */);
+
+}
+  /*private void SendNotification(string username, string message)
+    {
+        // Implement your notification sending logic here
+        // You may use AndroidNotificationCenter.SendNotification with a new channel for account activity warnings
+        // Retrieve the pet for the user (assuming you have a Pets class)
+var pet = Pets.GetPetForUser(userId);
+
+// Calculate minutes since last activity (replace this with your actual utility function)
+var minutesSinceLastActivity = CalculateTimeDifferenceInMinutes(pet.LastActivity);
+
+// 30 days in minutes
+var thirtyDaysInMinutes = 30 * 24 * 60;
+
+if (minutesSinceLastActivity > thirtyDaysInMinutes)
+{
+    SendNotification(user.Username, "Warning: Your account is at risk of termination. Log in to feed your pet!");
+}
+
+    }*/
+
