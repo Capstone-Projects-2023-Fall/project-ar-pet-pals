bb9d98857db5fbbbde870524a5c252ab95d9b1db
Anya Tewari <78163717+atewari18@users.noreply.github.com>
2023-11-27 23:16:59 -0500

    Update schema.user.ts

--- backend/src/schema/schema.user.ts
+++ backend/src/schema/schema.user.ts
@@ -1,10 +1,23 @@
-import {ObjectId} from "https://deno.land/x/mongo@v0.30.0/mod.ts";
+import { ObjectId } from "https://deno.land/x/mongo@v0.30.0/mod.ts";
+
+interface Step {
+  timestamp: Date;
+  steps: number;
+  healthScore: number;
+}
 
 export interface UserSchema {
-    _id: ObjectId;
-    username: string;
-    password: string;
-    token: string;
-    dateJoined: Date;
-    leaderboardRanking: number;
+  _id: ObjectId;
+  username: string;
+  password: string;
+  token: string;
+  dateJoined: Date;
+  leaderboardRanking: number;
+  dailyStepGoal: number;
+  dailyStepCount: number;
+  totalStepCount: number;
+  weeklyStepCount: number; 
+  stepCountScore: number; // New property
+  steps: Step[]; // New property
+birthday: Date; // Add the birthday property
 }
