28c59c67cab8498979ae8c789e3a44956473d045
atewari18 <78163717+atewari18@users.noreply.github.com>
2023-12-03 20:12:38 -0500

    trying to fix errors

--- backend/src/controllers/controller.steps.ts
+++ backend/src/controllers/controller.steps.ts
@@ -1,11 +1,29 @@
-// controllers.steps.ts
+import {ObjectId} from "https://deno.land/x/mongo@v0.32.0/mod.ts"
+import UserSchema from "../schema/schema.user.ts";
+import db from "../database/database.connection.ts";
+import { getUserIdFromHeaders } from "../utils/utils.utils.ts";
 
-import { Users } from "../database/database.connection.ts";
-import { ObjectId } from "https://deno.land/x/mongo@v0.30.0/mod.ts";
+const Users = db.collection<UserSchema>("users");
 
-export const updateStepCount = async (userId: ObjectId, steps: number) => {
-  const user = await Users.findOne({ _id: userId });
+export const updateStepCount = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const { steps } = await request.body().value;
+  const userId = getUserIdFromHeaders(request.headers);
 
+  const user = await Users.findOne({ _id: new ObjectId(userId) });
+
+  if (!user) {
+    response.body = {
+      message: "could not find user",
+    };
+    response.status = 400;
+    return;
+  }
   // Update daily step count
   user.dailyStepCount += steps;
 
@@ -18,9 +36,13 @@ export const updateStepCount = async (userId: ObjectId, steps: number) => {
   sevenDaysAgo.setDate(today.getDate() - 7);
 
   // Filter steps within the past seven days
-  const stepsWithinWeek = user.steps.filter(
-    (step) => step.timestamp >= sevenDaysAgo
-  );
+  let stepsWithinWeek: any[] = []
+
+  if(user.steps){
+    stepsWithinWeek = user.steps.filter(
+      (step) => step.timestamp >= sevenDaysAgo
+    );
+  }
 
   // Calculate the total steps within the past seven days
   user.weeklyStepCount = stepsWithinWeek.reduce(
@@ -31,23 +53,89 @@ export const updateStepCount = async (userId: ObjectId, steps: number) => {
   // Save the updated user data
   const result = await Users.updateOne({ _id: userId }, { $set: user });
 
-  return result;
+  if (!result) {
+    response.body = {
+      message: "could not update step count",
+    };
+    response.status = 400;
+    return;
+  }
+
+  response.body = {
+    message: "updated step count successfully",
+  };
+  response.status = 200;
+};
+export const getStepCount = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const headers: Headers = request.headers;
+  const userId = getUserIdFromHeaders(request.headers);
+
+  const user = await Users.findOne({ _id: new ObjectId(userId) });
+
+  if (!user) {
+    response.body = {
+      message: "could not find user",
+    };
+    response.status = 400;
+    return;
+  }
+  
+  response.body = {
+    dailyStepCount: user.dailyStepCount,
+    weeklyStepCount: user.weeklyStepCount
+  };
+  response.status = 200;
 };
 
-export const checkStepGoal = async (userId: ObjectId) => {
-  const user = await Users.findOne({ _id: userId });
+export const checkStepGoal = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const userId = getUserIdFromHeaders(request.headers);
+
+  const user = await Users.findOne({ _id: new ObjectId(userId) });
+
+  if (!user) {
+    response.body = {
+      message: "could not find user",
+    };
+    response.status = 400;
+    return;
+  }
 
   if (user.dailyStepCount >= user.dailyStepGoal) {
     // User met the daily step goal
-    return true;
-  } else {
-    // User did not meet the daily step goal
-    return false;
+    response.body = {
+      metDailyGoal: true,
+    };
+    response.status = 200;
+    return;
   }
+
+  response.body = {
+    metDailyGoal: false,
+  };
 };
 
-export const calculateAverageHealthScore = async (userId: ObjectId) => {
-  const user = await Users.findOne({ _id: userId });
+export const calculateAverageHealthScore = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const { userId, steps } = await request.body().value;
+
+  const user = await Users.findOne({ _id: new ObjectId(userId) });
 
   // Calculate the weekly step count
   const today = new Date();
@@ -60,11 +148,109 @@ export const calculateAverageHealthScore = async (userId: ObjectId) => {
   );
 
   // Calculate the total steps and health score within the past seven days
-  const totalSteps = stepsWithinWeek.reduce((total, step) => total + step.steps, 0);
-  const totalHealthScore = stepsWithinWeek.reduce((total, step) => total + step.healthScore, 0);
+  const totalSteps = stepsWithinWeek.reduce(
+    (total, step) => total + step.steps,
+    0
+  );
+  const totalHealthScore = stepsWithinWeek.reduce(
+    (total, step) => total + step.healthScore,
+    0
+  );
 
   // Calculate the average health score
   const averageHealthScore = totalHealthScore / (totalSteps / 1000); // Divide by 1000 for every 1000 steps
 
-  return averageHealthScore;
+  response.body = {
+    averageHealthScore,
+  };
+};
+
+export const updateStepGoal = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const { dailyStepGoal } = await request.body().value;
+  const userId = getUserIdFromHeaders(request.headers);
+
+  // Check if the user exists
+  const existingUser = await Users.findOne({ _id: new ObjectId(userId) });
+  if (!existingUser) {
+    response.body = { message: "User not found" };
+    response.status = 404;
+    return;
+  }
+
+  // Update the dailyStepGoal
+  const result = await Users.updateOne(
+    { _id: new ObjectId(userId) },
+    { $set: { dailyStepGoal } }
+  );
+
+  if (!result) {
+    response.body = { message: "Failed to update step goal" };
+    response.status = 500;
+    return;
+  }
+
+  response.body = { message: "Step goal updated successfully" };
+  response.status = 200;
+};
+
+export const resetDailyStepCountForAllUsers = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const { matchedCount, modifiedCount } = await Users.updateMany({
+    $set: {
+      dailyStepCount: 0,
+    },
+  });
+
+
+  if(modifiedCount ==0){
+    response.body = {
+      message: "could not reset daily step counter",
+    };
+    response.status = 400;
+    return;
+  }
+
+  response.body = {
+    message: "reset daily step counter",
+  };
+  response.status = 200;
+};
+
+export const resetWeeklyStepCountForAllUsers = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const { matchedCount, modifiedCount } = await Users.updateMany({},{
+    
+    $set: {
+      weeklyStepCount: 0,
+    },
+  });
+
+  if(modifiedCount ==0){
+    response.body = {
+      message: "could not reset weekly step counter",
+    };
+    response.status = 400;
+    return;
+  }
+  response.body = {
+    message: "reset weekly step counter",
+  };
+  response.status = 200;
+
 };
--- backend/src/routes/routes.allRoutes.ts
+++ backend/src/routes/routes.allRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import {signin, signup, getUserName, getUserInfo} from "../controllers/controllers.users.ts";
-import {setPetName, getPetName, setPetStatus, getPetStatus, resetPetStatus, createPet, setPetChoice, getPetChoice, } from "../controllers/controllers.pets.ts";
-import { recognizeFood } from "../controllers/controllers.food.ts";
+import {signin, signup, getUserName, getUserInfo, updateUser, deleteUser } from "../controllers/controllers.users.ts";
+import {setPetName, getPetName, setPetStatus, getPetStatus, resetPetStatus, createPet, setPetChoice, getPetChoice, increasePetMood, resetPetActivities } from "../controllers/controllers.pets.ts";
+import { recognizeFood, getHealthRating, getNutritionInfo, listFoodOptions } from "../controllers/controllers.food.ts";
 import {authourized} from "../middlewares/middlewares.isAuthorized.ts"
 import { verifyToken } from "../controllers/controllers.token.ts";
 import {getHealthRating } from "../controllers/controllers.health.ts";
-import { updateStepGoal } from "../controllers/controllers.users.ts";
+import { updateStepCount, checkStepGoal,getStepCount, updateStepGoal, resetDailyStepCountForAllUsers, resetWeeklyStepCountForAllUsers } from "../controllers/controllers.steps.ts";
+import { leaderboardList } from "../controllers/controllers.leaderboard.ts";
 
 
 const home = async({request, response}:{request:any;response:any}) => {
@@ -16,53 +17,79 @@ const router = new Router();
 
 //UNAUTHORIZED ROUTES
 router.get("/", home)
-router.post("/api/signup", signup )
-router.post("/api/signin",signin)
 
 // AUTHORIZED ROUTES
 
-//createPet
+// -- User --
+
+// Login and Registration
+router.post("/api/signup", signup)
+router.post("/api/signin", signin)
+router.post("/api/token/verify", verifyToken)
+
+// User Management
+router.get("/api/user/name", authourized, getUserName)
+router.put("/api/user", authourized, updateUser)
+router.delete("/api/user", authourized, deleteUser)
+router.get("/api/user", authourized, getUserInfo)
+
+// Update Step Goal
+router.put("/api/user/step-goal", authourized, updateStepGoal);
+
+// Update Step Count
+router.put("/api/user/step-count", authourized, updateStepCount);
+
+// get step count
+router.get("/api/user/step-count", authourized, getStepCount);
+
+// Check Step Goal
+router.get("/api/user/check-step-goal", authourized, checkStepGoal);
+
+router.get("/api/user/reset-daily-step-count",authourized, resetDailyStepCountForAllUsers )
+router.get("/api/user/reset-weekly-step-count", authourized, resetWeeklyStepCountForAllUsers )
+// -- Pet --
+
+// Create
 router.post("/api/pet/create", authourized, createPet  )
 
-//name
+// Name
 router.post("/api/pet/name", authourized, setPetName  )
 router.get("/api/pet/name", authourized, getPetName  )
 
-//status
+// Status
 router.post("/api/pet/status", authourized, setPetStatus  )
 router.get("/api/pet/status", authourized, getPetStatus  )
 router.post("/api/pet/status/reset", authourized, resetPetStatus  )
+router.put("/api/pet/status/increaseMood", authourized, increasePetMood  )
+router.put("/api/pet/activities/reset", authourized, resetPetActivities  )
 
-
-//username
-router.get("/api/user/name", authourized, getUserName  )
-
-//pet-choice
+// Choice
 router.post("/api/pet/choice", authourized, setPetChoice  )
 router.get("/api/pet/choice", authourized, getPetChoice )
 
+// -- Food --
 
-//userInfo
-router.get("/api/user", authourized, getUserInfo  )
+// Recognize
+router.post("/api/food/recognize", authourized, recognizeFood)
 
-// Update Step Goal
-router.put("/api/user/step-goal", authorized, updateStepGoal);
+// Nutrition Info
+router.post("/api/food/healthRating", authourized, getHealthRating);
+router.post("/api/food/nutritionInfo", authourized, getNutritionInfo);
 
-// Update Step Count
-router.put("/api/user/step-count", authorized, updateStep);
+// List Possible Foods
+router.get("/api/food/listFoodOptions", authourized, listFoodOptions);
 
-// Check Step Goal
-router.get("/api/user/check-step-goal", authorized, checkGoal);
+// -- Leaderboard --
 
-//verifies a token
-router.post("/api/token/verify", verifyToken)
+// User List 
+router.get("/api/leaderboard/list", leaderboardList)
+
 
 // food-related routes --
 
-// recognize food in image
-router.post("/api/food/recognize", authourized, recognizeFood)
 
-// get nutrition info for food
-router.post("/api/food/healthRating", authourized, getHealthRating);
+
+
+
 
 export default router;
