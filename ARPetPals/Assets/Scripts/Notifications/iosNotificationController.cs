86ce091cc3ffc671d2dc553c54dc06ba400ccb9f
atewari18 <78163717+atewari18@users.noreply.github.com>
2023-12-05 21:22:12 -0500

    making a generic ios notification

--- ARPetPals/Assets/Scripts/Notifications/iosNotificationController.cs
+++ ARPetPals/Assets/Scripts/Notifications/iosNotificationController.cs
@@ -1,7 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Notifications.Android;
+using Unity.Notifications.IOS;
 using UnityEngine;
 using UnityEngine.Andriod;
 
 public class iosNotificationController : MonoBehaviour
+{
+    public IEnumerator RequestAuthorization()
+    {
+        using var req = new AuthorizationRequest(AuthorizationOption.Alert | AuthorizationOption.Badge, true);
+        while(!req.IsFinished)
+        {
+            yield return null;
+        }
+    }
+
+    public void SendNotification(string title, string body, string subtitle, int FireTimeInSeconds)
+    {
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            TimeInterval = new TimeSpan(0, 0, FireTimeInSeconds),
+            Repeats = false
+        };
+
+        var notification = new iOSNotification()
+        {
+            Identifier = "generic_notification",
+            Title = title,
+            Body = body,
+            Subtitle = subtitle,
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "default_category",
+            ThreadIdentifier = "thread1",
+            timeTrigger = timeTrigger
+
+        };
+
+        iOSNotificationCenter.ScheduleNotification(notification);
+    }
+
+    }
+}
