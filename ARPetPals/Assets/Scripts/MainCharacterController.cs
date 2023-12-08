d5976e8d21b7c36af0e0cc61dce611265d8fd488
dariodaguanno <tul11082@temple.edu>
2023-12-07 19:13:28 -0500

    Added eating functions to SettingMenuController

--- ARPetPals/Assets/Scripts/MainCharacterController.cs
+++ ARPetPals/Assets/Scripts/MainCharacterController.cs
@@ -24,12 +24,6 @@ public class MainCharacterController : MonoBehaviour
 
     float health;
 
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        
-    }
-
     void Start()
     {
         changeMat();
