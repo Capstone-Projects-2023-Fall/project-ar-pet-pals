8d9d090cdae05f1264ddca114660cb5a91f99d47
son2005 <tranbaoson2005@gmail.com>
2023-12-12 18:46:38 -0500

    Fix unity compilation error

--- ARPetPals/Assets/Scripts/Notifications/MobileNotification.cs
+++ ARPetPals/Assets/Scripts/Notifications/MobileNotification.cs
@@ -10,7 +10,8 @@ public class MobileNotification : MonoBehaviour
         #if UNITY_ANDROID
         androidNotificationsController.RequestAuthorization();
         androidNotificationsController.RegisterNotificationChannel();
-        androidNotificationsController.SendNotification("ARPetPals: Have you met your step goal today? Open the app and give your pet a walk!", 15 );
+        // Son fix Unity compilation error - 3 args, not 2
+        androidNotificationsController.SendNotification("Notification!", "ARPetPals: Have you met your step goal today? Open the app and give your pet a walk!", 15 );
 
         #elif UNITY_IOS
         StartCoroutine(iosNotificationController.RequestAuthorization());
